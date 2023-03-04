f4ab1aed894864686cd7d3dc28a8062b305b2cf4
D1monPokem0n <ditch725@gmail.com>
2023-03-04 18:31:40 +0300

    Update, now we can work here

--- src/main/java/ru/ifmo/se/Main.java
+++ src/main/java/ru/ifmo/se/Main.java
@@ -7,8 +7,16 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        CommandReader rd = new CommandReader();
-        String command = rd.read();
-        System.out.println(command);
+        Storage storage = new Storage();
+        Controller controller = new Controller(storage);
+        CommandReader reader = new CommandReader();
+        while (controller.getProgramState().isWorkStatus()) {
+            String consoleString = reader.read();
+            String[] commandAndArgument = consoleString.split(" ");
+            String command = commandAndArgument[0];
+            String argument = commandAndArgument.length == 2 ? commandAndArgument[1] : null;
+            controller.setConsoleArgument(argument);
+            controller.executeCommand(command);
+        }
     }
 }
