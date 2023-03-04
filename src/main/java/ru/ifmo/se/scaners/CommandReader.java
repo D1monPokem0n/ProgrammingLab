ca75db16ac01dbd977c19348bc3db9471af8b4e3
D1monPokem0n <ditch725@gmail.com>
2023-03-04 18:25:28 +0300

    Upgrade Input

--- src/main/java/ru/ifmo/se/scaners/CommandReader.java
+++ src/main/java/ru/ifmo/se/scaners/CommandReader.java
@@ -6,17 +6,19 @@ import java.util.regex.Pattern;
 
 public class CommandReader {
     public String read() {
-        String commandsPattern = "help|info|show|add|update .*|remove_by_id .*|clear|save|execute_script .*|exit|add_if_min|remove_lower|history|filter_starts_with_comment .*|filter_less_than_event .*|print_descending";
+        String commandsPattern = "(help|info|show|add|update|remove_by_id|clear|save|execute_script|exit|add_if_min|remove_lower|history|filter_starts_with_comment|filter_less_than_event|print_descending).*";
         System.out.print("Введите команду: ");
         Scanner in = new Scanner(System.in);
-        rd:
         do {
-            String value = in.next();
-            if (Pattern.matches(commandsPattern, value)) {
+            String value = in.nextLine().trim();
+            boolean correctWordsNumber = (value.split(" ").length < 3);
+            boolean correctCommand = Pattern.matches(commandsPattern, value);
+            if (correctCommand & correctWordsNumber) {
                 return value;
+            } else if (correctCommand & !correctWordsNumber) {
+                System.out.print("Вы ввели слишком много аргументов или пробелов, повторите ввод: ");
             } else {
                 System.out.print("Вы ввели неверную команду, повторите ввод: ");
-                continue rd;
             }
         } while (true);
     }
