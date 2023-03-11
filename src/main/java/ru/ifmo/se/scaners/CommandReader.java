61ba697141817e3e64b5806bd733e4d294e110b8
D1monPokem0n <ditch725@gmail.com>
2023-03-11 17:25:59 +0300

    Take validation out

--- src/main/java/ru/ifmo/se/scaners/CommandReader.java
+++ src/main/java/ru/ifmo/se/scaners/CommandReader.java
@@ -1,24 +1,25 @@
 package ru.ifmo.se.scaners;
 
 import java.util.Scanner;
-import java.util.regex.Pattern;
 
 
 public class CommandReader {
-    public String read() {
-        String commandsPattern = "(help|info|show|add|update|remove_by_id|clear|save|execute_script|exit|add_if_min|remove_lower|history|filter_starts_with_comment|filter_less_than_event|print_descending).*";
+    private final ReadValidator validator;
+
+    public CommandReader() {
+        this.validator = new ReadValidator();
+    }
+
+    public String[] read() {
         System.out.print("Введите команду: ");
         Scanner in = new Scanner(System.in);
         do {
-            String value = in.nextLine().trim();
-            boolean correctWordsNumber = (value.split(" ").length < 3);
-            boolean correctCommand = Pattern.matches(commandsPattern, value);
-            if (correctCommand & correctWordsNumber) {
-                return value;
-            } else if (correctCommand & !correctWordsNumber) {
-                System.out.print("Вы ввели слишком много аргументов или пробелов, повторите ввод: ");
-            } else {
-                System.out.print("Вы ввели неверную команду, повторите ввод: ");
+            try {
+                String value = in.nextLine();
+                validator.setCheckedValue(value).validate();
+                return validator.format(value);
+            } catch (WrongCommandFormatException e){
+                System.out.print("Неверный формат команды, повторите ввод: ");
             }
         } while (true);
     }
